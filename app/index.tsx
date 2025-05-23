e75b390caf38f75a67793b7f8d0559389f3b7109
yuto <hungryyogibo1018@gmail.com>
2025-05-23 13:41:07 +0900

    背景を黒に変更

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,5 @@
-import { Text, View, Button } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Writing from "./writing";
+import Main from "./main";
 
 export default function Index() {
-  return <Writing />;
+  return <Main />;
 }
---
+++ app/main.tsx
@@ -0,0 +1,19 @@
+import { View } from "react-native";
+
+const Main = () => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "12%",
+        backgroundColor: "#696969",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: 1,
+      }}
+    ></View>
+  );
+};
+
+export default Main;
